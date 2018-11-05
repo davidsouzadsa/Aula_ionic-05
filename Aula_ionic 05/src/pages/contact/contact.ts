d6e4fdeba5fbf3c60408ec2b59eab26095e2b64d
davidsouzadsa <42626726+davidsouzadsa@users.noreply.github.com>
2018-11-04 23:37:54 -0200

    Prof Bonato

---
+++ Aula_ionic 05/src/pages/contact/contact.ts
@@ -0,0 +1,16 @@
+/* *@Author: David de Souza @RA: 201314824*/ 
+
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
